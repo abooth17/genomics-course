458f45ce2f2c49ed7916f0e2998e8bffb24f2fe6
abooth17 <albooth@umass.edu>
2020-08-31 10:42:19 -0400

    Lab 1

---
+++ lab1_AbigailBooth.R
@@ -0,0 +1,31 @@
+### Exercise 1 
+
+x <- 3
+y <- 15
+
+x + y
+x - y
+x * y
+x / y
+
+### Exercise 2
+
+x <- 2; y <- 3
+z <- x^5 > y^4
+z
+
+### Exercise 3
+
+vec <- c(22, 62, 148, 43, 129)
+vec_sum <- sum(vec)
+vec_sum
+freq <- c(vec / vec_sum)
+freq
+
+### Exercise 4
+ 
+nucleotides <- c("A", "T", "C", "G")
+ordered <- sort(nucleotides)
+ordered
+
+
